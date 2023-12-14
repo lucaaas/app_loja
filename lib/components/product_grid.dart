bff78cac5ea2d8aa6f34325abb680541792d9d46
Lucas <lucasoliveira2306@gmail.com>
2023-12-14 10:07:19 -0300

    product (refact): extract ProductGrid widget

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,27 @@
+import 'package:app_loja/components/product_item.dart';
+import 'package:app_loja/models/product.dart';
+import 'package:app_loja/providers/productProvider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ProductProvider provider = Provider.of<ProductProvider>(context);
+    final List<Product> products = provider.products;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (BuildContext context, int index) => ProductItem(product: products[index]),
+    );
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,33 +1,17 @@
-import 'package:app_loja/components/product_item.dart';
-import 'package:app_loja/models/product.dart';
-import 'package:app_loja/providers/productProvider.dart';
+import 'package:app_loja/components/product_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ProductProvider provider = Provider.of<ProductProvider>(context);
-    final List<Product> products = provider.products;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: products.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (BuildContext context, int index) => ProductItem(product: products[index]),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
